c3033f154bfdcff7a7862f028c6fb9d8531de2f7
Santiago Molano <santfirax@gmail.com>
2020-03-11 16:19:34 -0500

    done

--- src/main/java/birthday_cake_candles/Solution.java
+++ src/main/java/birthday_cake_candles/Solution.java
@@ -1,9 +1,53 @@
 package birthday_cake_candles;
 
-/**
- * @author José Santiago Molano
- * email: santfirax@gmail.com
- * 3/11/2020
- */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        List<Integer> candles = IntStream.of(ar).boxed().collect(Collectors.toList());
+        Integer maxHeight = Collections.max(candles);
+        int count = candles.stream()
+                .filter(candle -> candle.equals(maxHeight))
+                .mapToInt(e -> 1).sum();
+        System.out.println(count);
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
